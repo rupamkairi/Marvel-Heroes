b83107cab4a86b64655bf0ac5f4a62fad546450b
Rupam Kairi <rupamkairi@hotmail.com>
2020-03-31 12:56:08 +0530

    secrets compatible

--- scripts/main.js
+++ scripts/main.js
@@ -10,9 +10,9 @@ async function Req() {
   event.preventDefault();
   querry.name = document.getElementById("search_input").value;
   let timestamp = Date.now();
-  let hash = CryptoJS.MD5(timestamp + user.private_key + user.public_key);
+  let hash = CryptoJS.MD5(timestamp + secrets.private_key + secrets.public_key);
 
-  let url = `http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${user.public_key}&hash=${hash}&name=${querry.name}`;
+  let url = `http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${secrets.public_key}&hash=${hash}&name=${querry.name}`;
   //   &limit=${querry.limit.toString()}&offset=${querry.offset.toString()}`;
   console.log(url);
 
